579c72177f461e432addef3149b72572f119d134
Dianne Reyes <dianne_reyes@baylor.edu>
2023-02-25 10:24:08 -0600

    adding the bullet settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's settings. Controls game's appearance and ship's speed"""
         
         #Screen settings
         self.screen_width = 1200
@@ -11,3 +11,9 @@ class Settings:
 
         #adj. ship speed - ship speed will now move 1.5 pixels rather than 1 pixel on each pass through the loop 
         self.ship_speed = 1.5
+
+        #adding the bullets setting - create dark gray bullets with a width of 3 px and height of 15 px; speed < ship speed
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
